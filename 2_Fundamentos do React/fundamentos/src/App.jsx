ef25af39d1892f551dd973bd15e5d4106b2f441c
Breno Otávio <brenolemesd@gmail.com>
2026-04-07 11:35:37 -0300

    revisão fundamentos

--- 2_Fundamentos do React/fundamentos/src/App.jsx
+++ 2_Fundamentos do React/fundamentos/src/App.jsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
+import { useState } from "react";
 //Styles
-import './App.css'
-//components | 
-import FirstComponent from './components/FirstComponent'
-import TemplateExpressions from './components/TemplateExpressions'
-import Events from './components/Eventos'
-import Challenge from './components/Challenge'
-
-
-
-
+import "./App.css";
+//components |
+import FirstComponent from "./components/FirstComponent";
+import TemplateExpressions from "./components/TemplateExpressions";
+import Events from "./components/Eventos";
+import Challenge from "./components/Challenge";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <h1>Fundamentos React</h1>
         {/* Chamando o arquivo <FirstComponent/> */}
-        <FirstComponent/>
-        <TemplateExpressions/>
-        <Events/>
+        <FirstComponent />
+        <TemplateExpressions />
+        <Events />
         <Challenge />
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 2_Fundamentos do React/fundamentos/src/components/Eventos.jsx
+++ 2_Fundamentos do React/fundamentos/src/components/Eventos.jsx
@@ -1,32 +1,28 @@
 const Events = () => {
-
-    const renderSomething = (x) => {
-
-        if(x) {
-            return <h1>Renderizando aqui</h1>
-        } else {
-            return <h1>Ou isso aqui</h1>
-        }
-
+  const renderSomething = (x) => {
+    if (x) {
+      return <h1>Renderizando aqui</h1>;
+    } else {
+      return <h1>Ou isso aqui</h1>;
     }
+  };
+  //e => dados que o evento possui
+  const handleMyEvent = (e) => {
+    console.log(e);
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleMyEvent}>Clique aqui!</button>
+        <button onClick={() => console.log("Clicou!")}>
+          {" "}
+          Clique aqui e olhe o console
+        </button>
+      </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
+    </div>
+  );
+};
 
-    //e => dados que o evento possui
-    const handleMyEvent = (e) => {
-        console.log(e)
-    }
-    return(
-
-        <div>
-            <div>
-                <button onClick={handleMyEvent}>Clique aqui!</button>
-                <button onClick={() => console.log("Clicou!")}> Clique aqui e olhe o console</button>
-            </div>
-            {renderSomething(true)}
-            {renderSomething(false)}
-        </div>
-
-    )
-
-}
-
-export default Events
+export default Events;
