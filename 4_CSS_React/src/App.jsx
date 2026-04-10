299e904fa87eec9347d5901c79321b5f7562fa41
Breno Otávio <brenolemesd@gmail.com>
2026-04-10 14:03:16 -0300

    css inline

--- 4_CSS_React/src/App.jsx
+++ 4_CSS_React/src/App.jsx
@@ -5,10 +5,23 @@ export default function App() {
   return (
     <div className="App">
       {/* CSS global */}
-      <h1>React com c0ss</h1>
+      <h1>React com css</h1>
       {/* CSS de componente */}
       <MyComponents />
       <p>Este p é do app.js</p>
+      {/* inline css */}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado inline
+      </p>
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
+        Este elemento foi estilizado inline
+      </p>
     </div>
   );
 }
